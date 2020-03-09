2ca107503899503bf400bfa955dddb673b9b3d8b
root <sok.kimheng@gmail.com>
2020-03-09 10:12:48 +0700

    Process Communication

---
+++ pysocket/client.py
@@ -0,0 +1,23 @@
+# Chat client
+import socket
+import os
+
+server = '127.0.0.1'
+port = 2000
+
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server, port))
+    
+        s.sendall('get'.encode('utf8'))
+        print(s.recv(4096).decode('utf8'))
+
+    new_msg = input('Send Message: ')
+    
+    if new_msg:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((server, port))
+            s.sendall(f'send{new_msg}\n'.encode('utf8'))
+        if new_msg == 'exit': exit(0)
---
+++ pysocket/server.py
@@ -0,0 +1,39 @@
+#https://realpython.com/python-sockets
+#https://github.com/realpython/materials/tree/master/python-sockets-tutorial
+# Chat server
+import socket
+
+messages = []
+HOST ='127.0.0.1'#IP or Name
+PORT =2000 #1-65535 if port <1024 superuser require in some system
+#AF_INET: IPV4 (HOST,PORT) , TCP: SOCK_STREAM
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #BIND
+    s.bind((HOST, PORT))
+    #LISTEN
+    s.listen()
+    print('-- SERVER IS ONLINE --')
+
+    while True:
+        #ACCEPT
+        conn, addr = s.accept()
+        with conn:
+            #print('Connection from', addr)
+            
+            while True:
+                #RECEIVE Buffer size =1024
+                data = conn.recv(1024).decode('utf8') 
+                if data:
+                    if data.startswith('send'):
+                        messages.append(f'{addr[0]}: {data[4:]}')
+                        print(data[4:])
+                        m=data[4:8]
+                        if m == 'exit': 
+                              exit(0)
+                    elif data.startswith('get'):
+                        #SEND
+                        conn.sendall('\n'.join(messages[-10:]).encode('utf8'))
+                    break
+     #CLOSE
+     #No need to call s.close() uses "pass"
+                    
